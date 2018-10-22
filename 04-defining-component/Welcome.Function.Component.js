5b3ad109a3e99e33fc938a100021f93fd21b9b39
gulshansainis <gulshan.saini@gmail.com>
2018-10-22 23:26:40 +0530

    defining component as function or class

---
+++ 04-defining-component/Welcome.Function.Component.js
@@ -0,0 +1,8 @@
+/*
+* This function is a valid React component 
+* - it accepts a single “props”
+* - returns a React element. 
+*/
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
